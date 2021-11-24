95fce0c7e3099d37ee8581fc448dea4945754b09
Ismael Shakverdiev <st.bahahd@gmail.com>
2021-11-24 19:44:14 +0400

    Add tokens for lexer

---
+++ lib/src/tokens.dart
@@ -0,0 +1,13 @@
+// Operation signs.
+const addition = "+";
+const subtraction = '-';
+const multiplication = '*';
+const division = '/';
+const percentage = '%';
+
+// Point signs
+const comma = ',';
+const point = '.';
+
+// Variable tokens.
+double num(double n) => n;
