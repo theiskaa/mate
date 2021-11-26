1ac6ebe256e2bebc5d71c02e21f6747131654d94
Ismael Shakverdiev <st.bahahd@gmail.com>
2021-11-26 15:42:23 +0400

    Add new example & configure UI implementation
    - Added new dart implementation example of mate.
    - Configured UI/Flutter implementation example of mate.

---
+++ example/app.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:mate/mate.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const MateView(),
+    );
+  }
+}
+
+class MateView extends StatefulWidget {
+  const MateView({Key? key}) : super(key: key);
+
+  @override
+  _MateViewState createState() => _MateViewState();
+}
+
+class _MateViewState extends State<MateView> {
+  final Mate mate = Mate();
+  final expressionController = TextEditingController();
+
+  String? result = '0';
+
+  void calculate() {
+    final String expression = expressionController.text;
+
+    // Calculate expression.
+    final res = mate.calculate(expression);
+
+    // Alert if expression is invalid.
+    if (res == null) {
+      setState(() => result = 'Invalid\nExpression');
+      return;
+    }
+
+    setState(() => result = res.toString());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mate')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              result.toString().replaceAll('.0', ''),
+              style: const TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: expressionController,
+                decoration: const InputDecoration(
+                  hintText: 'Expression',
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: calculate,
+              child: const Text('Calculate'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- example/main.dart
+++ example/main.dart
@@ -1,85 +1,28 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
+import 'package:mate/mate.dart';
 
-// import 'package:mate/mate.dart';
+/*
+  This file is just a simple implementation example of mate.
 
-// void main() => runApp(const App());
+  See official documentation/explanation of working style of mate. - [https://github.com/theiskaa/mate#readme]  
+  And here is UI/Flutter implementation of mate - [https://github.com/theiskaa/mate/blob/main/example/app.dart]
 
-// class App extends StatelessWidget {
-//   const App({Key? key}) : super(key: key);
+*/
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: ThemeData.dark(),
-//       home: const ExpressionParserView(),
-//     );
-//   }
-// }
+void main() {
+  // Create new mate instance.
+  final Mate mate = Mate();
 
-// class ExpressionParserView extends StatefulWidget {
-//   const ExpressionParserView({Key? key}) : super(key: key);
+  const expressions = [
+    '2+2', // 4
+    '-2 + 5 + 10 * 2', // 23
+    '10 % 2 + 0.8 + 19 / 2 - 0.5', // 10
+    "*2abc-10/%" // null (invalid),
+  ];
 
-//   @override
-//   _ExpressionParserViewState createState() => _ExpressionParserViewState();
-// }
+  for (var expression in expressions) {
+    // use [calculate] function, to parse, and calculate expression.
+    final result = mate.calculate(expression);
 
-// class _ExpressionParserViewState extends State<ExpressionParserView> {
-//   final ExpressionParser expressionParser = ExpressionParser();
-//   final operationController = TextEditingController();
-
-//   String? result = '0';
-
-//   void calculate() {
-//     final String operation = operationController.text;
-
-//     // We can catch expression's validness like:
-//     if (expressionParser.isInvalidExp(operation)) {
-//       setState(() => result = 'Invalid\nOperation');
-//       return;
-//     }
-
-//     // Calulcate operation if it's valid.
-//     final res = expressionParser.calculate(operation);
-
-//     setState(() => result = res.toString());
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('Mate')),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text(
-//               result.toString().replaceAll('.0', ''),
-//               style: const TextStyle(
-//                 fontSize: 50,
-//                 fontWeight: FontWeight.bold,
-//               ),
-//               textAlign: TextAlign.center,
-//             ),
-//             const SizedBox(height: 50),
-//             Padding(
-//               padding: const EdgeInsets.symmetric(horizontal: 20),
-//               child: TextField(
-//                 controller: operationController,
-//                 decoration: const InputDecoration(
-//                   hintText: 'Operation',
-//                   border: InputBorder.none,
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 30),
-//             ElevatedButton(
-//               onPressed: calculate,
-//               child: const Text('Calculate'),
-//             )
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+    print(result);
+  }
+}
