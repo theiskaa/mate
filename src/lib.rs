6d7e82b46465ec164180c4d3c50aa2e440e9e67b
Ismael Shakverdiev <me@theiskaa.com>
2022-09-19 21:51:23 +0400

    feat: add crate description to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,78 @@
+/*!
+This crate provides a library for parsing and calculating arithmetic expressions inputted as &str(string).
+Uses `Lexer` structure to parse string input in to token list, and `Calculator` structure to calculate final result from token list.
+Has also a general wrapper structure that implements `Lexer` and `Calculator` inside of it. And makes it easy to calculate arithmetic
+expression's result directly without dealing with parsing and calculating manually.
+
+# Usage
+
+This crate is on [crates.io](http://crates.io/crates/mate-rs) and can be used by adding regex to your dependencies in your project's `Cargo.toml`.
+```toml
+[dependencies]
+mate-rs = "0.1.0"
+```
+
+# Example: Simple usage | `Mate`
+
+`Mate` is general wrapper structure for `Lexer` and `Calculator`.
+has only one method that used to `calculate` result via string(&str) input.
+
+```rust
+use mate_rs::mate::Mate;
+
+let result = Mate::calculate("6 * 7");
+match result {
+    Ok(v) => assert_eq!(v, 42.0),
+    Err(_) => {
+        // Do something ...
+    }
+};
+```
+
+# Example: Complicated usage | `Lexer` and `Calculator`
+
+`Lexer` is the main structure that parses string-input to token-list.
+`Calculator` is the structure that used to calculate final result via `Lexer`'s result.
+
+```rust
+use mate_rs::{calculator::Calculator, lexer::Lexer};
+
+// Generates tokens gonna be something like:
+//  | Token(type: NUMBER  literal: "-2"),
+//  | Token(type: PLUS    literal: "+"),
+//  | Token(type: NUMBER  literal: "2"),
+//  | Token(type: PLUS    literal: "+"),
+//  | Token(
+//  |   type: SUBEXP,
+//  |   tokens: [
+//  |        Token(type: NUMBER,  literal: "6")
+//  |        Token(type: PRODUCT, literal: "*")
+//  |        Token(type: NUMBER,  literal: "7")
+//  |   ],
+//  | ),
+let tokens = Lexer::lex(" - 2 + 2 + 6 * 7").unwrap(); // should handle error case also
+
+// Result will be calculated from tokens, by X/O/Y algorithm.
 //
-// Copyright 2022-present theiskaa. All rights reserved.
-// Use of this source code is governed by MIT license
-// that can be found in the LICENSE file.
+//  ╭────────╮ ╭───────────╮ ╭────────╮
+//  │ NUMBER │ │ OPERATION │ │ NUMBER │
+//  ╰────────╯ ╰───────────╯ ╰────────╯
+//       ╰───╮       │        ╭───╯
+//           ▼       ▼        ▼
+//           X  [+, -, *, /]  Y
 //
+let result = Calculator::calculate(tokens);
+
+match result {
+    Ok(v) => assert_eq!(v, 42.0),
+    Err(_) => {
+        // Do something ...
+    }
+};
+```
+
+> For details refer to [repository](https://github.com/theiskaa/mate).
+*/
 
 pub mod calculator;
 pub mod errors;
