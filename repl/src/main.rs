2d03170bcf46ddbaeb4dcdc4b51fb1cf0cbcfd76
Ismael Sh <me@theiskaa.com>
2024-01-11 23:17:38 +0400

    feat(repl): add inline calculation
    Resolves: https://github.com/theiskaa/mate/issues/25

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -6,11 +6,22 @@
 
 use colored::Colorize;
 use mate_rs::{calculator::Calculator, lexer::Lexer, monitor::Monitor, token::Token};
-use std::io::{stdin, stdout, Write};
+use std::{io::{stdin, stdout, Write}, env};
 
 fn main() {
     // TODO: take me from command line arguments
     let log_tokens: bool = false;
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        let input = args
+            .iter()
+            .skip(1)
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>()
+            .join(" ");
+        return execute_calculator(input.as_str(), log_tokens);
+    }
 
     loop {
         let mut input: String = String::new();
@@ -20,24 +31,26 @@ fn main() {
 
         match stdin().read_line(&mut input) {
             Err(e) => println!("{} {} \n", format!("[!]").bold().red(), e.to_string().red()),
-            Ok(_) => {
-                let sub = match Lexer::lex(input.clone().as_str()) {
-                    Ok(tt) => tt,
-                    Err(e) => return print_err(e.to_string()),
-                };
-
-                if log_tokens {
-                    print_tokens(sub.tokens.clone())
-                }
-
-                let result = Calculator::calculate(sub, &input.clone().as_str());
-                match result {
-                    Ok(v) => println!("{} \n", v.to_string().green().bold()),
-                    Err(e) => print_err(e.to_string()),
-                };
-            }
+            Ok(_) => execute_calculator(input.clone().as_str(), log_tokens),
         };
     }
+}
+
+fn execute_calculator(input: &str, log_tokens: bool) {
+    let sub = match Lexer::lex(input) {
+        Ok(tt) => tt,
+        Err(e) => return print_err(e.to_string()),
+    };
+
+    if log_tokens {
+        print_tokens(sub.tokens.clone())
+    }
+
+    let result = Calculator::calculate(sub, &input);
+    match result {
+        Ok(v) => println!("{} \n", v.to_string().green().bold()),
+        Err(e) => print_err(e.to_string()),
+    };
 }
 
 fn print_err(msg: String) {
